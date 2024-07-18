72bab5f88ebd1099f3310d3012f9f976be2cd1c6
lukilme <kilmerluisluk12@gmail.com>
2024-07-18 19:34:43 -0300

    :bug: fixing somethings

---
+++ build.ps1
@@ -0,0 +1,78 @@
+# Definir variáveis
+$diretorioAtual = Get-Location
+Write-Host "Diretorio atual: $diretorioAtual"
+
+
+git add -A
+
+
+$mensagemCommit = Read-Host -Prompt "Insira uma mensagem de commit"
+
+
+try {
+    git commit -m $mensagemCommit
+    Write-Host "Commit realizado com sucesso!"
+} catch {
+    Write-Host "Erro ao realizar o commit. Verifique os arquivos e tente novamente."
+    exit
+}
+
+
+$resposta = Read-Host -Prompt "Deseja enviar o commit? (y/n)"
+
+if ($resposta -eq "y") {
+    try {
+        git push
+        Write-Host "Commit enviado com sucesso!"
+    } catch {
+        Write-Host "Erro ao enviar o commit. Verifique sua conexão e tente novamente."
+        exit
+    }
+
+
+    ng build --output-path docs --base-href /pweb-angular-study/
+    $sourceFolder = "C:\Users\Desktop\Desktop\lukilme\college\pweb-angular\docs\browser"
+    $destinationFolder = "C:\Users\Desktop\Desktop\lukilme\college\pweb-angular\dist"
+
+    if (!(Test-Path $sourceFolder)) {
+        Write-Host "A pasta de origem '$sourceFolder' não existe."
+        exit
+    }
+
+    
+    if (!(Test-Path $destinationFolder)) {
+        New-Item -ItemType Directory -Path $destinationFolder
+    }
+
+    
+    try {
+        Copy-Item -Path $sourceFolder\* -Destination $destinationFolder -Force
+        Write-Host "Arquivos copiados com sucesso de '$sourceFolder' para '$destinationFolder'."
+    } catch {
+        Write-Host "Erro ao copiar arquivos. Verifique os caminhos e permissões."
+        exit
+    }
+
+ 
+    try {
+        Remove-Item -Path $sourceFolder -Recurse
+        Write-Host "Pasta de origem removida com sucesso."
+    } catch {
+        Write-Host "Erro ao remover a pasta de origem. Verifique os caminhos e permissões."
+        exit
+    }
+
+    try {
+        ngh --dir=docs
+        Write-Host "Publicação no GitHub Pages realizada com sucesso."
+    } catch {
+        Write-Host "Erro ao publicar no GitHub Pages. Verifique as configurações e tente novamente."
+        exit
+    }
+
+} elseif ($resposta -eq "n") {
+    git reset --soft HEAD^
+    Write-Host "Commit desfeito."
+} else {
+    Write-Host "Resposta inválida. Nenhuma ação foi tomada."
+}
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -12,7 +12,22 @@ import {MatToolbarModule} from '@angular/material/toolbar';
 })
 
 export class HeaderComponent {
-  toggleMode() {
+  delay_const: number = 0.1; 
+  toggleMode():void {
     document.body.classList.toggle('dark-theme');
   }
+
+  piscapisca(): void {
+    this.delay_const += 0.1;
+    const elementosGayLetter = document.querySelectorAll('.gay-letter');
+    if (this.delay_const > 1) {
+        this.delay_const = 0.1;
+    }
+    
+    elementosGayLetter.forEach((elemento, i) => {
+        let atraso: number;
+        atraso = (i * this.delay_const) % 1;  // Modulo para garantir que o atraso está entre 0 e 1
+        (elemento as HTMLElement).style.animationDelay = `${atraso}s`;
+    });
+}
 }
