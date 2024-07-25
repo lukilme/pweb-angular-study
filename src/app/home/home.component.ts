983c520a7ffce2b2060cf9ddd4ec9dd45db050d7
lukilme <kilmerluisluk12@gmail.com>
2024-07-25 19:37:09 -0300

    ::lipstick: style tabs

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,53 +1,63 @@
-import { Component, AfterViewInit, OnDestroy, OnInit  } from '@angular/core';
-import { interval, Subscription } from 'rxjs';
+import { Component, AfterViewInit, OnDestroy, OnInit } from "@angular/core";
+import { count, interval, Subscription } from "rxjs";
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.scss"],
 })
-
 export class HomeComponent implements AfterViewInit {
   private subscription: Subscription = new Subscription();
   public seconds: number = 0;
   public gay_emoji: string = "✨";
-  
+
   painting_text(element: HTMLElement) {
     if (element) {
-      const text = element.textContent || '';
-      element.innerHTML = '';
+      const text = element.textContent || "";
+      element.innerHTML = "";
 
       for (let i = 0; i < text.length; i++) {
-        const span = document.createElement('span');
+        const span = document.createElement("span");
         span.textContent = text[i];
-        span.className = 'gay-letter';
+        span.className = "gay-letter";
         element.appendChild(span);
       }
     }
   }
 
   ngAfterViewInit(): void {
-    const textElements = document.getElementsByClassName('gay-text') as HTMLCollectionOf<HTMLElement>;
-    Array.from(textElements).forEach(item => {
-      this.painting_text(item)
-  
+    const textElements = document.getElementsByClassName(
+      "gay-text"
+    ) as HTMLCollectionOf<HTMLElement>;
+    Array.from(textElements).forEach((item) => {
+      this.painting_text(item);
     });
   }
 
   ngOnInit(): void {
-    const timer$ = interval(1000);
-    this.subscription = timer$.subscribe(() => {
-      this.seconds++;
-      if(this.seconds%2==0){
-        this.gay_emoji = "😎";
-      }else{
-        this.gay_emoji = "✨";
-      }
-    });
+    const text_to_change: HTMLElement | null = document.getElementById("praticando");
+      const timer$ = interval(500);
+      let counter = 0;
+      this.subscription = timer$.subscribe(() => {
+        this.seconds++;
+        counter++;
+        switch (counter) {
+          case 3:
+            text_to_change!.textContent="Praticando Angular...";
+            break;
+          case 2:
+            text_to_change!.textContent="Praticando Angular..";
+            break;
+          case 1:
+            text_to_change!.textContent="Praticando Angular.";
+            break;
+          default:
+            counter = 0;
+        }
+      });
+    
   }
 
   ngOnDestroy(): void {
     this.subscription.unsubscribe();
   }
-
-  
 }
--- src/app/subpart/container/container.component.ts
+++ src/app/subpart/container/container.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 import {MatTabsModule} from '@angular/material/tabs';
 import { Entity1Component } from '../entity1/entity1.component';
 import { Entity2Component } from '../entity2/entity2.component';
@@ -8,6 +8,7 @@ import { Entity3Component } from '../entity3/entity3.component';
   templateUrl: './container.component.html',
   styleUrl: './container.component.scss',
   standalone: true,
+  encapsulation: ViewEncapsulation.None, // <------ This is Important
   imports: [MatTabsModule, Entity1Component,Entity2Component, Entity3Component],
 })
 export class ContainerComponent {
